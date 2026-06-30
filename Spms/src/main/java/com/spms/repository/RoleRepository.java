78d40d41b58c3ff2222a6fe19e78856298f39688
Aaloka123 <aalokapoudel10@gmail.com>
2026-06-30 23:13:12 +0545

    Add Role entity and repository

---
+++ Spms/src/main/java/com/spms/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.spms.repository;
+
+import com.spms.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByRoleName(String roleName);
+
+}
